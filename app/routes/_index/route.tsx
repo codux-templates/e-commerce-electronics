a36422f6dd16a10b493ed1749d469608630609cc
Codux <codux@codux.com>
2024-12-17 12:45:01 +0200

    purchase

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -15,8 +15,8 @@ export default function HomePage() {
     return (
         <div>
             <section className={styles.hero}>
-                <h1 className={styles.header2}>Shop The Top Brand Electronicy.</h1>
-                <p className={styles.p1}>Get more for your money with every purchese!</p>
+                <h1 className={styles.header2}>Shop The Top Brand Electronic.</h1>
+                <p className={styles.p1}>Get more for your money with every purchase!</p>
                 <LabelWithArrow
                     className={styles.labelWithArrow}
                     onClick={anchorToNewIn}
