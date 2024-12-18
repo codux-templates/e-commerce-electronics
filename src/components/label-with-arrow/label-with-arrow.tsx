3e5ebc34aadaf2a306d447c15faa1917a982f7fb
Codux <codux@codux.com>
2024-12-18 11:30:20 +0200

    b

--- src/components/label-with-arrow/label-with-arrow.tsx
+++ src/components/label-with-arrow/label-with-arrow.tsx
@@ -6,19 +6,22 @@ import { FC, HTMLAttributes } from 'react';
 export interface LabelWithArrowProps {
     onClick?(): void;
     className?: string;
+    /** @visualizer color */
     bgColor1?: string;
+    /** @visualizer color */
     bgColor2?: string;
     verticalSpacing?: number;
     horizontalSpacing?: number;
     text?: string;
 }
-export const LabelWithArrow = ({
+export const LabelWithArrow = ({ 
     onClick,
     className,
     bgColor1,
     bgColor2,
     text,
     verticalSpacing,
+
     horizontalSpacing,
 }: LabelWithArrowProps) => {
     const [animationBegin, setAnimationBegin] = useState(0);
@@ -34,7 +37,7 @@ export const LabelWithArrow = ({
             onClick={onClick}
             onAnimationEnd={onAnimationEnd}
             style={{
-                '--bgColor1': bgColor1 ? `${bgColor1}` : '#000000',
+                '--bgColor1': bgColor1 ? `${bgColor1}` : '#000009',
                 '--bgColor2': bgColor2 ? `${bgColor2}` : '#ffffff',
                 '--verticalSpacing': verticalSpacing ? `${verticalSpacing}px` : '20px',
                 '--horizontalSpacing': horizontalSpacing ? `${horizontalSpacing}px` : '20px',
