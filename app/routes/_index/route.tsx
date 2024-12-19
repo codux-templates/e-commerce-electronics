dc51f9ff25bbc3399b6aeeb975ced72a170f306b
Codux <codux@codux.com>
2024-12-19 12:32:43 +0200

    ij

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
     return (
         <div>
             <section className={styles.hero}>
-                <h1 className={styles.header2}>Shop The Top Brand Electronic.</h1>
+                <h1 className={styles.header2}>Shop The Top Brand Electronix.</h1>
                 <p className={styles.p1}>Get more for your money with every purchase!</p>
                 <LabelWithArrow
                     className={styles.labelWithArrow}
