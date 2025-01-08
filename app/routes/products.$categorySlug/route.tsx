bcab600c128ffda772dd6a0301b78b81c07b7273
Codux <codux@codux.com>
2025-01-08 12:23:42 +0200

    cat page grid

--- app/routes/products.$categorySlug/route.tsx
+++ app/routes/products.$categorySlug/route.tsx
@@ -146,9 +146,7 @@ export default function ProductsPage() {
                         <h1 className={styles.categoryName}>
                             {appliedFilters.search ? `"${appliedFilters.search}"` : category.name}
                         </h1>
-                        {category.description && !appliedFilters.search && (
-                            <p className={styles.categoryDescription}>{category.description}</p>
-                        )}
+                        <p className={styles.p1}>Mobile Devices.</p>
                     </div>
 
                     {someFiltersApplied && (
